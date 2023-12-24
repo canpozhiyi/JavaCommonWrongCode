888777241373bca60a476f5ac5f6a40df40319c5
csd <1014384908@qq.com>
2023-12-24 23:25:51 +0800

    project init

---
+++ src/main/java/com/csd/wrongCode/Application.java
@@ -0,0 +1,19 @@
+package com.csd.wrongCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author chensd
+ * @Date 2023-12-24
+ **/
+
+@SpringBootApplication
+public class Application {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
